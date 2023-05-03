96ec1285dd5a60c3ba88549e8852aabe9cf71d47
olegguralnik15@gmail.com <olegguralnik15@gmail.com>
2023-05-03 06:23:35 +0000

    tiktok done

---
+++ models/staging/stg_tiktok.sql
@@ -0,0 +1,28 @@
+select 
+    `date`, 
+    add_to_cart, 
+    clicks, 
+    0 as comments, 
+    0 as engagements, 
+    impressions, 
+    rt_installs as installs, 
+    0 as likes, 
+    0 as link_clicks, 
+    0 as post_click_conversions,
+    0 as post_view_conversions,
+    0 as posts,
+    purchase,
+    registrations,
+    0 as revenue,
+    0 as shares,
+    spend,
+    0 as total_conversions,
+    video_views,
+    cast(ad_id as string) as ad_id,
+    cast(adgroup_id as string) as adset_id,
+    cast(campaign_id as string) as campaign_id,
+    channel,
+    null as creative_id,
+    null as placement_id
+
+from {{ ref('src_ads_tiktok_ads_all_data') }}
